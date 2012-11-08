9f17a41770dfdbcddae4b200439b47ff7378b47b
Bediko <bediko@chaosdorf.de>
2012-11-08 16:32:04 +0100

    Add syntax py

---
+++ syntax.py
@@ -0,0 +1,42 @@
+
+def main_token:
+def procedure_token:
+def program_token:
+def end_token:
+def parameter_token:
+def integer_token:
+def string_token:
+def input_token:
+def inout_token:
+def in_token:
+def out_token:
+def declaration_token:
+def exitloop_token:
+def print_token:
+def set_token:
+def add_token:
+def sub_token:
+def conctat_token:
+def call_token:
+def langle_token:
+def rangle_token:
+def loop_token:
+def case_token:
+def when_token:
+def otherwise_token:
+def less_token:
+def equal_token:
+def greater_token:
+def constant_token:
+def digit_token:
+def identifier_token:
+
+
+
+
+
+
+
+def syntax_analyze(tokens):
+
+    return code
