5ba25c481c7967a87e14c85d8c64ea0470635d5d
Bediko <bediko@chaosdorf.de>
2012-11-22 14:48:39 +0100

    Add statement function

--- syntax.py
+++ syntax.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 code = ''
-statements=[]
+statements = ["PRINT", "INPUT", "SET", "ADD", "SUB", "CONCAT", "CALL"]
 
 
 def pop(tokens):
@@ -144,7 +144,27 @@ def controlflow_token(tokens):
     elif tokens[0][0] == "LOOP":
         tokens = loop_token(tokens)
     elif tokens[0][0] == "CASE":
-        tokens = case_token(tokens) 
+        tokens = case_token(tokens)
+    elif tokens[0][0] in statements:
+        tokens = statement_token(tokens)
+    else:
+        unknown_token(tokens)
+    return tokens
+
+
+def statement_token(tokens):
+    if tokens[0][0] == "PRINT":
+        tokens = print_stmt_token(tokens)
+    elif tokens[0][0] == "INPUT":
+        tokens = input_stmt_token(tokens)
+    elif tokens[0][0] == "SET":
+        tokens = assign_stmt_token(tokens)
+    elif tokens[0][0] == "ADD" or tokens[0][0] == "SUB":
+        tokens = integer_operation_token(tokens)
+    elif tokens[0][0] == "CONCAT":
+        tokens = string_operation_token(tokens)
+    elif tokens[0][0] == "CALL":
+        tokens = call_stmt_token(tokens)
     else:
         unknown_token(tokens)
     return tokens
@@ -152,7 +172,9 @@ def controlflow_token(tokens):
 
 def exitloop_token(tokens):
     return pop(tokens)
-# def print_token(tokens):
+
+
+#def print_token(tokens):
 # def set_token(tokens):
 # def add_token(tokens):
 # def sub_token(tokens):
@@ -170,8 +192,15 @@ def exitloop_token(tokens):
 # def constant_token(tokens):
 # def digit_token(tokens):
 
+
 def identifier_token(tokens):
     return pop(tokens)
+
+
+def unknown_token(tokens):
+    print 'Unknown token: ' + tokens[0][1] + ' on line ' \
+        + str(tokens[0][2])
+    exit(0)
 
 
 def syntax_analyze(tokens):
